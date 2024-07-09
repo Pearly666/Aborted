c99bbe90e708ed50f8ee8a25d46e3278cd5d7657
Bitton Sarah <sarah.corres@outlook.fr>
2024-07-09 15:51:51 +0200

    doors final version I guess

---
+++ Assets/#Project/Scripts/SaltBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltBehaviour : MonoBehaviour, IInteractable
+{
+    public ProjectileSalt saltScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform saltContainer;
+    
+    
+    public void Interact()
+    {
+
+    }
+}
